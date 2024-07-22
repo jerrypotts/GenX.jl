465eb55226c1e3eae2770a2802532ac48fdb9222
Jerry Potts <jerry.potts@utexas.edu>
2024-07-22 10:52:30 -0600

    add PSIP dependency

--- src/GenX.jl
+++ src/GenX.jl
@@ -37,6 +37,7 @@ using Random
 using RecursiveArrayTools
 using Statistics
 using HiGHS
+using PowerSystemsInvestmentsPortfolios
 
 # Global scaling factor used when ParameterScale is on to shift values from MW to GW
 # DO NOT CHANGE THIS (Unless you do so very carefully)
